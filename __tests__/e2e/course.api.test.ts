4a1bf875b2ca6f7a75e2a7fd8f5f034257405ee2
Maxim <benyachef@gmail.com>
2023-04-19 15:45:24 +0300

    jest test complete

--- __tests__/e2e/course.api.test.ts
+++ __tests__/e2e/course.api.test.ts
@@ -29,24 +29,54 @@ describe('/course', () => {
             .get('/courses')
             .expect(HTTP_STATUS.OK_200, [])
     })
+    let createdCourse1: any = null;
+    it('should create course with correct input data', async () => {
 
+        const createResponse = await request(app)
+            .post('/courses/')
+            .send({title: 'it-incubator course 1'})
+            .expect(HTTP_STATUS.CREATED_201);
 
+        createdCourse1 = createResponse.body;
 
-    it('should`nt create course with incorrect input data', async () => {
+        expect(createdCourse1).toEqual({
+            id: expect.any(Number),
+            title: 'it-incubator course 1'
+        })
 
         await request(app)
-            .put('/courses/' + createCourse.id)
+            .get('/courses')
+            .expect(HTTP_STATUS.OK_200, [createdCourse1])
+    })
+    let createdCourse2: any = null;
+    it('create one more course', async () => {
+        const createResponse = await request(app)
+            .post('/courses')
+            .send({title: 'it-incubator course 2'})
+            .expect(HTTP_STATUS.CREATED_201)
+        createdCourse2 =createResponse.body;
+
+        expect(createdCourse2).toEqual({
+            id: expect.any(Number),
+            title: 'it-incubator course 2'
+        })
+
+        await request(app)
+            .get('/courses')
+            .expect(HTTP_STATUS.OK_200, [createdCourse1, createdCourse2])
+    })
+    it('should`nt update course with incorrect data', async () => {
+        await request(app)
+            .put('/courses/' + createdCourse1.id)
             .send({title: ''})
             .expect(HTTP_STATUS.BAD_REQUEST_400);
 
-
         await request(app)
-            .get('/courses/' + createCourse.id)
-            .expect(HTTP_STATUS.OK_200, createCourse)
+            .get('/courses/' + createdCourse1.id)
+            .expect(HTTP_STATUS.OK_200, createdCourse1)
     })
 
     it('should`nt update course that not exist', async () => {
-
         await request(app)
             .put('/courses/' + -100)
             .send({title: 'good title'})
@@ -54,38 +84,42 @@ describe('/course', () => {
     })
 
     it('should update course with correct input data', async () => {
-
         await request(app)
-            .put('/courses/' + createCourse.id)
+            .put('/courses/' + createdCourse1.id)
             .send({title: 'good new title'})
             .expect(HTTP_STATUS.NO_CONTENT_204);
 
         await request(app)
-            .get('/courses/' + createCourse.id)
+            .get('/courses/' + createdCourse1.id)
             .expect(HTTP_STATUS.OK_200, {
-                ...createCourse,
+                ...createdCourse1,
                 title: 'good new title'
             })
+
+        await request(app)
+            .get('/courses/' + createdCourse2.id)
+            .expect(HTTP_STATUS.OK_200, createdCourse2)
     })
 
-    let createCourse: any = null;
-    it('should create course with correct input data', async () => {
+    it('should delete both course', async () => {
+        await request(app)
+            .delete('/courses/' + createdCourse1.id)
+            .expect(HTTP_STATUS.NO_CONTENT_204)
 
-        const createResponse = await request(app)
-            .post('/courses')
-            .send({title: 'it-incubator course'})
-            .expect(HTTP_STATUS.CREATED_201);
+        await request(app)
+            .get('/courses/' + createdCourse1.id)
+            .expect(HTTP_STATUS.NOT_FOUND_404)
 
-        createCourse = createResponse.body;
+        await request(app)
+            .delete('/courses/' + createdCourse2.id)
+            .expect(HTTP_STATUS.NO_CONTENT_204)
 
-        expect(createCourse).toEqual({
-            id: expect.any(Number),
-            title: 'it-incubator course'
-        })
+        await request(app)
+            .get('/courses/' + createdCourse2.id)
+            .expect(HTTP_STATUS.NOT_FOUND_404)
+
         await request(app)
             .get('/courses')
-            .expect(HTTP_STATUS.OK_200, [createCourse])
+            .expect(HTTP_STATUS.OK_200, [])
     })
-
-
 })
